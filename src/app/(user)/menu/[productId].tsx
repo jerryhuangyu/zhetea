0c38f9ed28314bab29f392ccc3a471c23ef83d67
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-23 22:05:46 +0800

    feat: Update product detail page with scrollview and add2cart button

--- src/app/(user)/menu/[productId].tsx
+++ src/app/(user)/menu/[productId].tsx
@@ -1,7 +1,8 @@
 import { Stack, useLocalSearchParams } from "expo-router";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View, ScrollView } from "react-native";
 import { useState } from "react";
 import AmountSelector from "@/src/components/selector/AmountSelector";
+import BasicBtn from "@/src/components/button/BasicBtn";
 import { teaProducts } from "@/assets/data";
 import { Amount } from "@/types";
 
@@ -20,13 +21,20 @@ const DetailPage = () => {
   }
 
   return (
-    <View style={styles.container}>
+    <>
       <Stack.Screen options={{ title: product.name }} />
-      <Image source={{ uri: product.image }} style={styles.image} />
-      <AmountSelector selected={selected} setSelected={setSelected} />
-
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+      <ScrollView>
+        <View style={styles.scrollview}>
+          <Image source={{ uri: product.image }} style={styles.image} />
+          <AmountSelector selected={selected} setSelected={setSelected} />
+          <Text style={styles.price}>${product.price}</Text>
+          <Text>{product.description}</Text>
+        </View>
+      </ScrollView>
+      <View style={styles.btnDiv}>
+        <BasicBtn text="Add to cart" />
+      </View>
+    </>
   );
 };
 
@@ -35,18 +43,25 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
     flex: 1,
+  },
+  scrollview: {
+    paddingHorizontal: 10,
+    flex: 1,
     gap: 10,
-    padding: 10,
+    flexGrow: 1,
   },
   image: {
     width: "100%",
     aspectRatio: 1,
-    borderEndEndRadius: 8,
-    borderEndStartRadius: 8,
+    borderRadius: 10,
   },
   price: {
     fontSize: 18,
     fontWeight: "700",
-    marginTop: "auto",
+  },
+  btnDiv: {
+    paddingTop: 2,
+    paddingBottom: 2,
+    paddingHorizontal: 10,
   },
 });
