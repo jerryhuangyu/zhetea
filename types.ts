c7b3c9034162249fdf8234e76d2468e9fb00ad14
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-23 23:43:41 +0800

    feat: Setup order relative type

--- types.ts
+++ types.ts
@@ -14,3 +14,23 @@ export type CartItem = {
   amount: Amount;
   quantity: number;
 };
+
+export type OrderStatus = "New" | "Cooking" | "Delivering" | "Delivered";
+
+export type Order = {
+  id: number;
+  created_at: string;
+  total: number;
+  user_id: string;
+  status: OrderStatus;
+  order_items?: OrderItem[];
+};
+
+export type OrderItem = {
+  id: number;
+  product_id: number;
+  products: Product;
+  order_id: number;
+  amount: Amount;
+  quantity: number;
+};
