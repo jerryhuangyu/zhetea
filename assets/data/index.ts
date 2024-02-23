92c4d9328197b7ffcbce4d4e847cbd2f6f21cfcc
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-23 18:38:43 +0800

    refactor: Add dummy tea product data

---
+++ assets/data/index.ts
@@ -0,0 +1,92 @@
+export const teaProduct = [
+  {
+    id: 1,
+    name: "綠茶",
+    image: "https://picsum.photos/id/386/200",
+    price: 14.99,
+  },
+  {
+    id: 2,
+    name: "紅茶",
+    image: "https://picsum.photos/id/33/200",
+    price: 12.99,
+  },
+  {
+    id: 3,
+    name: "白茶",
+    image: "https://picsum.photos/id/22/200",
+    price: 16.99,
+  },
+  {
+    id: 4,
+    name: "烏龍茶",
+    image: "https://picsum.photos/id/93/200",
+    price: 10.99,
+  },
+  {
+    id: 5,
+    name: "草本茶",
+    image: "https://picsum.photos/id/84/200",
+    price: 19.99,
+  },
+  {
+    id: 6,
+    name: "伯爵茶",
+    image: "https://picsum.photos/id/99/200",
+    price: 8.99,
+  },
+  {
+    id: 7,
+    name: "茉莉花茶",
+    image: "https://picsum.photos/id/55/200",
+    price: 11.99,
+  },
+  {
+    id: 8,
+    name: "洋甘菊茶",
+    image: "https://picsum.photos/id/43/200",
+    price: 15.99,
+  },
+  {
+    id: 9,
+    name: "薄荷茶",
+    image: "https://picsum.photos/id/25/200",
+    price: 13.99,
+  },
+  {
+    id: 10,
+    name: "大吉嶺茶",
+    image: "https://picsum.photos/id/117/200",
+    price: 17.99,
+  },
+  {
+    id: 11,
+    name: "抹茶",
+    image: "https://picsum.photos/id/120/200",
+    price: 9.99,
+  },
+  {
+    id: 12,
+    name: "普洱茶",
+    image: "https://picsum.photos/id/121/200",
+    price: 18.99,
+  },
+  {
+    id: 13,
+    name: "紅茶",
+    image: "https://picsum.photos/id/233/200",
+    price: 12.99,
+  },
+  {
+    id: 14,
+    name: "紅茶",
+    image: "https://picsum.photos/id/195/200",
+    price: 12.99,
+  },
+  {
+    id: 15,
+    name: "紅茶",
+    image: "https://picsum.photos/id/311/200",
+    price: 12.99,
+  },
+];
