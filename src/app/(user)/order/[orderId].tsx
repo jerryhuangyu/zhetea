02e438db316b4e9112409729943c3cddcbb7faef
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-24 00:49:41 +0800

    feat: Create basic order detail page

---
+++ src/app/(user)/order/[orderId].tsx
@@ -0,0 +1,33 @@
+import orders from "@/assets/data/orders";
+import { OrderDisplayCard } from "@/src/components/OrderCard";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+const OrderDetailScreen = () => {
+  const { orderId } = useLocalSearchParams();
+  const order = orders.find((i) => i.id.toString() === orderId);
+
+  if (!order) {
+    return <Text>Not found</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: `Order #${orderId}` }} />
+      <OrderDisplayCard order={order} />
+      <FlatList
+        data={order.order_items}
+        renderItem={({ item }) => <Text>{item.products.name}</Text>}
+        contentContainerStyle={{ gap: 10 }}
+      />
+    </View>
+  );
+};
+export default OrderDetailScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    gap: 10,
+  },
+});
