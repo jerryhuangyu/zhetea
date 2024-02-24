f61ac0eef6cd65b04c36fd2cd61fe54c36b9c156
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-24 17:37:54 +0800

    feat: Create order detail page for admin

---
+++ src/app/(admin)/order/[orderId].tsx
@@ -0,0 +1,45 @@
+import { Stack, useLocalSearchParams } from "expo-router";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { OrderDisplayCard } from "@/src/components/card/OrderCard";
+import OrderDetailCard from "@/src/components/card/OrderDetailCard";
+import orders from "@/assets/data/orders";
+import Colors from "@/constants/Colors";
+import StatusSelector from "@/src/components/selector/StatusSelector";
+
+const OrderDetailScreen = () => {
+  const { orderId } = useLocalSearchParams();
+  const order = orders.find((i) => i.id.toString() === orderId);
+
+  if (!order) {
+    return <Text>Not found</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen
+        options={{
+          title: `Order #${orderId}`,
+          headerTintColor: Colors.light.tint,
+          headerTitleStyle: { color: "black" },
+        }}
+      />
+      <OrderDisplayCard order={order} highlight />
+      <FlatList
+        data={order.order_items}
+        renderItem={({ item }) => <OrderDetailCard item={item} />}
+        contentContainerStyle={{ gap: 10 }}
+        ListFooterComponentStyle={{ paddingTop: 5 }}
+        ListFooterComponent={() => <StatusSelector order={order} />}
+      />
+    </View>
+  );
+};
+export default OrderDetailScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    gap: 10,
+    flex: 1,
+  },
+});
